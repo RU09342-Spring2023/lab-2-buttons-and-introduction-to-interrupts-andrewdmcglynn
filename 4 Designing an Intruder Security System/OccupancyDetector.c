ee8e1cc15df0a97afade46e127cdce68f3d981bc
Andrew <mcglyn44@students.rowan.edu>
2023-02-13 14:41:06 -0500

    Added name and started main function for part4

--- 4 Designing an Intruder Security System/OccupancyDetector.c
+++ 4 Designing an Intruder Security System/OccupancyDetector.c
@@ -2,8 +2,17 @@
  * OccupancyDetector.c
  *
  *  Created on: Jan 30, 2023
- *      Author: russty
+ *      Author: Andrew McGlynn
  */
+#include <msp430.h>
+
+int main(void)
+{
+    WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
+    //Configure GPIO
+    P1OUT &= ~BIT0;                         // Clear P1.0 output latch for a defined power-on state
+       P1DIR |= BIT0;
+}
 
 
 
